0cc115cb04fbefe25e34ddf57a955e4be178e430
iamananya <ananyaiit123@gmail.com>
2022-05-15 21:11:27 +0530

    done

--- src/apiClient/apiClient.js
+++ src/apiClient/apiClient.js
@@ -2,16 +2,26 @@ import axios from "axios";
 
 const baseUrl = 'http://localhost:8000';
 
-export const createBatch = async (batch_start, batch_code, course) => {
-    var body = {
-        'batch_start' : batch_start, 
-        'batch_code' : batch_code,
-        'course' : course
-    }
-    await axios.post(baseUrl + '/batches/', body)
-}
-
 export const getBatches = async () => {
     const res = await axios.get(baseUrl + '/batches/')
+    return res
+}
+
+export const getVideos = async () => {
+    const res = await axios.get(baseUrl + '/courses/')
+    return res
+}
+export const getMaterial = async () => {
+    const res = await axios.get(baseUrl + '/material/')
+    return res
+}
+
+export const getNotif = async () => {
+    const res = await axios.get(baseUrl + '/notif/')
+    return res
+}
+
+export const getStudents = async () => {
+    const res = await axios.get(baseUrl + '/profiles/')
     return res
 }
--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -4,120 +4,22 @@ import Timetable from "./Timetable";
 import { Navigation } from "react-minimal-side-navigation";
 import "react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css";
 import { BrowserRouter ,Route} from "react-router-dom";
-
+import ListGroup from 'react-bootstrap/ListGroup'
+import {Link} from "react-router-dom"
 function Sidenav() {
   return (
     <div>
-      <Navigation
-        // you can use your own router's api to get pathname
-        activeItemId="/management/members"
-        onSelect={({ itemId }) => {
-          // maybe push to the route
-          // history.push(itemId);
-        }}
-        items={[
-        
-          {
-            title: "Overview",
-            itemId: "/timetable",
-            // you can use your own custom Icon component as well
-            // icon is optional
-            // elemBefore: () => <Icon name="inbox" />,
-          },
-          {
-            title: "Attendance",
-            itemId: "/timetable",
-            // you can use your own custom Icon component as well
-            // icon is optional
-            // elemBefore: () => <Icon name="inbox" />,
-          },
-          {
-            title: "Assignment",
-            itemId: "/timetable",
-            // you can use your own custom Icon component as well
-            // icon is optional
-            // elemBefore: () => <Icon name="inbox" />,
-          },
-          {
-            title: "Test",
-            itemId: "/timetable",
-            // you can use your own custom Icon component as well
-            // icon is optional
-            // elemBefore: () => <Icon name="inbox" />,
-          },
-          {
-            title: "Announcement",
-            itemId: "/timetable",
-            // you can use your own custom Icon component as well
-            // icon is optional
-            // elemBefore: () => <Icon name="inbox" />,
-          },
-          {
-            title: "Video",
-            itemId: "/timetable",
-            // you can use your own custom Icon component as well
-            // icon is optional
-            // elemBefore: () => <Icon name="inbox" />,
-          },
-          {
-            title: "Live Class",
-            itemId: "/timetable",
-            // you can use your own custom Icon component as well
-            // icon is optional
-            // elemBefore: () => <Icon name="inbox" />,
-          },
-          {
-            title: "Study Material",
-            itemId: "/timetable",
-            // you can use your own custom Icon component as well
-            // icon is optional
-            // elemBefore: () => <Icon name="inbox" />,
-          },
-          {
-            title: "Settings",
-            itemId: "/timetable",
-            // you can use your own custom Icon component as well
-            // icon is optional
-            // elemBefore: () => <Icon name="inbox" />,
-          },
-          
-          {
-            title: "Students",
-            itemId: "/management",
-            // elemBefore: () => <Icon name="users" />,
-            subNav: [
-              {
-                title: "Video",
-                itemId: "/management/projects",
-              },
-              {
-                title: "Settings",
-                itemId: "/management/members",
-              },
-            ],
-          },
-          {
-            title: "Setting",
-            itemId: "/another",
-            subNav: [
-              {
-                title: "Teams",
-                itemId: "/management/teams",
-              },
-            ],
-          },
-          {
-            title: "Video",
-            itemId: "/another",
-            subNav: [
-              {
-                title: "Teams",
-                itemId: "/management/teams",
-              },
-            ],
-          },
-        ]}
-      />
+      
+         
+
+      <ListGroup style={{fontSize:"20px",color:"3B8854"}} >
+    <ListGroup.Item><Link style={{textDecoration:"none"}} to="/batch">Video</Link></ListGroup.Item>
+    <ListGroup.Item><Link style={{textDecoration:"none"}} to="/material">Study Material</Link></ListGroup.Item>
+    <ListGroup.Item><Link style={{textDecoration:"none"}} to="/profiles">Students</Link></ListGroup.Item>
+    
+    
+   
+  </ListGroup>
     </div>
   );
 }
